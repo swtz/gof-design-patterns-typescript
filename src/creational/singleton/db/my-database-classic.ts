4221fd3c1b01ecda2861a2d96e18150b539223d3
Leonardo Lüders <luderser@hotmail.com>
2025-09-17 10:01:40 -0300

    Created in-memory database and its operations for MyDatabaseClassic (classic singleton).

--- src/creational/singleton/db/my-database-classic.ts
+++ src/creational/singleton/db/my-database-classic.ts
@@ -1,18 +1,41 @@
+interface User {
+  name: string;
+  age: number;
+}
+
 export class MyDatabaseClassic {
   private static instance: MyDatabaseClassic | null = null;
+  private users: User[] = [];
 
   private constructor() {}
 
-  public static getInstance(): MyDatabaseClassic {
+  static getInstance(): MyDatabaseClassic {
     if (MyDatabaseClassic.instance === null) {
       MyDatabaseClassic.instance = new MyDatabaseClassic();
     }
 
     return MyDatabaseClassic.instance;
   }
+
+  add(user: User): void {
+    this.users.push(user);
+  }
+
+  remove(index: number): void {
+    this.users.splice(index, 1);
+  }
+
+  show(): void {
+    for (const user of this.users) {
+      console.log(user);
+    }
+  }
 }
 
-const db1 = MyDatabaseClassic.getInstance();
-const db2 = MyDatabaseClassic.getInstance();
+const myDbClassic = MyDatabaseClassic.getInstance();
 
-console.log(db1 === db2);
+myDbClassic.add({ age: 35, name: 'Maria' });
+myDbClassic.add({ age: 53, name: 'Luiz' });
+myDbClassic.add({ age: 65, name: 'Léo' });
+myDbClassic.remove(1);
+myDbClassic.show();
