105a70b253d8ae3189af8195f4f6551015069695
Leonardo Lüders <luderser@hotmail.com>
2025-09-19 08:44:31 -0300

    (Factory Method) Created Product and ConcreteProduct participants.

---
+++ src/creational/factory-method/client-code.ts
@@ -0,0 +1,9 @@
+import { Car } from './vehicle/car';
+
+const fusca = new Car('fusca');
+fusca.pickUp('Leonard');
+fusca.stop();
+
+const celta = new Car('celta');
+celta.pickUp('Mary');
+celta.stop();
