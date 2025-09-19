3aa900a1f010ffe7a60895ef05dbaa6ce13e8d3d
Leonardo Lüders <luderser@hotmail.com>
2025-09-19 09:38:57 -0300

    Created randomCarAlgorithm to generate Vehicle objects.

--- src/creational/factory-method/client-code.ts
+++ src/creational/factory-method/client-code.ts
@@ -1,11 +1,18 @@
 import { CarFactory } from './factories/car-factory';
+import { randomCarAlgorithm } from './main/random-vehicle-algorithm';
+import { randomNumbers } from './utils/random-numbers';
 
 const carFactory = new CarFactory();
+const customerNames = ['Ana', 'Joana', 'Helena', 'João'];
 
-const fusca = carFactory.getVehicle('fusca');
-fusca.pickUp('Leonard');
-fusca.stop();
-
-const celta = carFactory.getVehicle('celta');
-celta.pickUp('Mary');
-celta.stop();
+for (let i = 0; i < 10; i++) {
+  const vehicle = randomCarAlgorithm();
+  const name = customerNames[randomNumbers(customerNames.length)];
+  vehicle.pickUp(name);
+  vehicle.stop();
+  // Abaixo, um exemplo do uso dos métodos implementados na
+  // classe abstrata VehicleFactory
+  const newCar = carFactory.pickUp(name, `CARRO Nº: ${randomNumbers(100)}`);
+  newCar.stop();
+  console.log('---');
+}
--- src/creational/factory-method/vehicle/bicycle.ts
+++ src/creational/factory-method/vehicle/bicycle.ts
@@ -4,7 +4,7 @@ export class Bicycle implements Vehicle {
   constructor(private name: string) {}
 
   pickUp(customerName: string): void {
-    console.log(`${this.name} está buscando o ${customerName}`);
+    console.log(`${this.name} está buscando ${customerName}`);
   }
   stop(): void {
     console.log(`${this.name} parou`);
